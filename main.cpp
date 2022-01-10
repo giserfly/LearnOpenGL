f31a9edc29a4ed9ef5771c22199aa7e29031069a
unknown <zhangpengfei@geoway.net>
2022-01-10 22:39:11 +0800

    Hello Triangle

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+
+
 int main()
 {
 	// glfw: initialize and configure
@@ -43,6 +45,90 @@ int main()
 		return -1;
 	}
 
+
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	const char *fragmentShaderSource = "#version 330 core \n"
+		"out vec4 FragColor; \n"
+		"void main()\n"
+		"{\n"
+		"FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+		"}\n";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	//蓝色着色器
+	const char *fragmentShaderSourceBlue = "#version 330 core \n"
+		"out vec4 FragColor; \n"
+		"void main()\n"
+		"{\n"
+		"FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
+		"}\n";
+
+	unsigned int fragmentShaderBlue = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderBlue, 1, &fragmentShaderSourceBlue, NULL);
+	glCompileShader(fragmentShaderBlue);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	unsigned int shaderProgramBlue = glCreateProgram();
+	glAttachShader(shaderProgramBlue, vertexShader);
+	glAttachShader(shaderProgramBlue, fragmentShaderBlue);
+	glLinkProgram(shaderProgramBlue);
+	//glDeleteShader(vertexShader);
+	//glDeleteShader(fragmentShader);
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	float vertices2[] = {
+		0.0f,  0.5f, 0.0f,
+		 0.5f, 0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f
+	};
+
+	unsigned int VBO;
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	//第二个图层
+	unsigned int VBO2;
+	unsigned int VAO2;
+	glGenVertexArrays(1, &VAO2);
+	glGenBuffers(1, &VBO2);
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 	// render loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -51,6 +137,20 @@ int main()
 		// -----
 		processInput(window);
 
+		// render
+	   // ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//draw second
+		
+		glUseProgram(shaderProgramBlue);
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
